d9106402dbb955636fe6ba21ae0ba04f182a573e
Sahibzada Issar Ali <issaralisia@gmail.com>
2025-04-16 11:53:52 +0500

    change

---
+++ script.js
@@ -0,0 +1,14 @@
+const autoTyping = document.getElementById("auto-typing");
+const options = {
+  typeSpeed: 50,
+  deleteSpeed: 50,
+  waitBeforeDelete: 2000,
+  waitBetweenWords: 500,
+};
+
+const typing = new AutoTyping(
+  "#auto-typing",
+  ["A Web Developer", "and Designer"],
+  options
+);
+typing.start();
